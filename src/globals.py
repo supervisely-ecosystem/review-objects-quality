3b01e06753363da9c7f05cbb47aa051e9504dc71
Alex Emelyanov <alex84_84_84@mail.ru>
2021-10-19 15:26:16 +0300

    run compare-gallery with 2 imgs

--- src/globals.py
+++ src/globals.py
@@ -3,10 +3,12 @@ import supervisely_lib as sly
 
 my_app = sly.AppService()
 
+api: sly.Api = my_app.public_api
+task_id = my_app.task_id
+
 TEAM_ID = int(os.environ['context.teamId'])
 WORKSPACE_ID = int(os.environ['context.workspaceId'])
 PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
 DATASET_ID = int(os.environ['modal.state.slyDatasetId'])
-
 
 
--- src/main.py
+++ src/main.py
@@ -23,7 +23,23 @@ def send_error_data(func):
 @send_error_data
 def test_compary_gallery(api: sly.Api, task_id, context, state, app_logger):
 
-    pass
+    project_info = api.project.get_info_by_id(g.PROJECT_ID)
+    meta_json = api.project.get_meta(project_info.id)
+    meta = sly.ProjectMeta.from_json(meta_json)
+    dataset_info = api.dataset.get_info_by_id(g.DATASET_ID)
+
+    images = api.image.get_list(dataset_info.id)
+    image_ids = [image_info.id for image_info in images]
+    ann_infos = api.annotation.download_batch(g.DATASET_ID, image_ids)
+    anns = [sly.Annotation.from_json(ann_info.annotation, meta) for ann_info in ann_infos]
+
+    gallery = CompareGallery(g.task_id, g.api, 'data.perClass', meta)
+    gallery.set_left(title='image 1', ann=anns[0],
+                              image_url=api.image.get_info_by_id(image_ids[0]).full_storage_url)
+    gallery.set_right(title='image 2', ann=anns[0],
+                               image_url=api.image.get_info_by_id(image_ids[0]).full_storage_url)
+    gallery.update()
+
 
 
 def main():
@@ -31,7 +47,7 @@ def main():
         "context.teamId": g.TEAM_ID
     })
 
-    g.my_app.run()
+    g.my_app.run(initial_events=[{"command": "test_compary_gallery"}])
 
 
 if __name__ == "__main__":
